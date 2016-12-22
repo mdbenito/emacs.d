868c6a8746e0070501deb1fd327490b3e9e59dc9
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2016-12-22 10:30:29 +0100

    Update elpa

---
+++ elpa/cmake-ide-20161221.455/cmake-ide-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-ide" "20161221.455" "Calls CMake to find out include paths and other compiler flags" '((emacs "24.1") (cl-lib "0.5") (seq "1.11") (levenshtein "0")) :stars '(#("126" 0 3 (font-lock-face paradox-star-face))) :url "http://github.com/atilaneves/cmake-ide" :keywords '("languages"))
---
+++ elpa/rtags-20161216.1019/rtags-pkg.el
@@ -0,0 +1,4 @@
+(define-package "rtags" "20161216.1019" "A front-end for rtags" 'nil :url "http://rtags.net")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
