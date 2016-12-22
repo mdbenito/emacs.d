e25622b5c96d32f9012b48099fb1ae4a8b3d8627
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2016-12-22 10:29:44 +0100

    Comment

--- config/navigation.el
+++ config/navigation.el
@@ -1,9 +1,5 @@
-;; "When several buffers visit identically-named files,
-;; Emacs must give the buffers distinct names. The usual method
-;; for making buffer names unique adds ‘<2>’, ‘<3>’, etc. to the end
-;; of the buffer names (all but one of them).
-;; The forward naming method includes part of the file's directory
-;; name at the beginning of the buffer name
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Make repeating buffer names unique by prepending path chunks as needed
 ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Uniquify.html
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
