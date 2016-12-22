96c53388329f8306aee61e56a80c0fca316b8c2e
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2016-12-22 10:05:22 +0100

    Fix reading of paradox token

--- config/misc.el
+++ config/misc.el
@@ -13,12 +13,21 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Remote access
-(defun get-string-from-file (file-path)
-;; thanks to Pascal J Bourguignon and TheFlyingDutchman <zzbba…@aol.com>
-  "Return filePath's file content."
+(defun mbd-string-chomp (str)
+  "Chomp leading and tailing whitespace from STR.
+From: https://www.emacswiki.org/emacs/ElispCookbook"
+  (replace-regexp-in-string (rx (or (: bos (* (any " \t\n")))
+                                    (: (* (any " \t\n")) eos)))
+                            ""
+                            str))
+(defun mbd-string-from-file (file-path)
+  "Return filePath's file content.
+Pascal J Bourguignon and TheFlyingDutchman <zzbba…@aol.com>"
   (with-temp-buffer
     (insert-file-contents file-path)
-    (buffer-string)))
+    (mbd-string-chomp (buffer-string))))
 
-(setq tramp-default-method "ssh")
-(setq paradox-github-token (get-string-from-file "paradox-token"))
+(eval-after-load 'tramp
+  (setq tramp-default-method "ssh"))
+(eval-after-load 'paradox
+  (setq paradox-github-token (mbd-string-from-file "~/.emacs.d/private/paradox-token")))
