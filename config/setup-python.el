de13fd1e236802cd251a3e033a1e48c9be095a02
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2016-12-31 19:22:28 +0100

    pdb shortcut

--- config/setup-python.el
+++ config/setup-python.el
@@ -7,3 +7,6 @@
 (add-hook 'python-mode-hook #'anaconda-mode)
 (eval-after-load "company"
   '(add-to-list 'company-backends 'company-anaconda))
+
+(eval-after-load 'python-mode
+  (define-key python-mode-map (kbd "s-d") #'pdb))
