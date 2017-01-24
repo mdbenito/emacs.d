66b4dbdc1f6197a3c46b49951628ac3d0fbe6bcd
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-01-24 11:28:50 +0100

    Fix conflicting shortcut in markdown mode

--- config/setup-markdown.el
+++ config/setup-markdown.el
@@ -1,3 +1,6 @@
+;; Keymap conflicts
 (with-eval-after-load 'markdown-mode
   (define-key markdown-mode-map (kbd "M-<left>") nil)
-  (define-key markdown-mode-map (kbd "M-<right>") nil))
+  (define-key markdown-mode-map (kbd "M-<right>") nil)
+  (define-key markdown-mode-map (kbd "M-n") nil) ;; M-n is ~ in german Apple keyboard
+  )
