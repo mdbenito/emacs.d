f10899bfe1151733f23378429f35a97c5dd6566c
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-01-24 11:30:08 +0100

    Update lots of packages

--- elpa/request-20161221.1711/request-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "request" "20161221.1711" "Compatible layer for URL request in Emacs" '((emacs "24") (cl-lib "0.5")) :stars '(#("6" 0 1 (font-lock-face paradox-star-face))))
