9e07f11ff7b6448b2b88b4f6408e366c0d25c71f
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-03-16 13:12:36 +0100

    Enable and config rtags

--- config/setup-cpp.el
+++ config/setup-cpp.el
@@ -11,3 +11,13 @@
 ;; Specify extra flags forwarded to compiler
 ;(setq cppcm-extra-preprocss-flags-from-user '("-I/usr/src/linux/include" "-DNDEBUG"))
 
+;; Enable rtags to run over tramp
+(require 'rtags)
+(setq rtags-tramp-enabled t)
+(setq rtags-autostart-diagnostics t)
+(setq rtags-completions-enabled t)
+;(require 'company)
+;(global-company-mode)
+(add-to-list 'company-backends 'company-rtags)
+
+(cmake-ide-setup)
