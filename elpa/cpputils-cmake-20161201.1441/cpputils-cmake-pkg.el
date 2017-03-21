1e1413cb64dadff59c7e0ed1f8318bd1b4b1680c
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-03-21 12:00:15 +0100

    Update packages

--- elpa/cpputils-cmake-20161201.1441/cpputils-cmake-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "cpputils-cmake" "20161201.1441" "Easy real time C++ syntax check and intellisense if you use CMake" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/modern-cpp-font-lock-20160622.1321/modern-cpp-font-lock-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "modern-cpp-font-lock" "20160622.1321" "Font-locking for \"Modern C++\"" (quote nil))
