19f8b4f03b295e420aff7d10b2aee7abd878a720
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-04-11 23:20:13 +0200

    Fixes for c++ setup. Far from optimal

--- config/setup-cpp.el
+++ config/setup-cpp.el
@@ -1,20 +1,20 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Basic setup
+
+(require 'flycheck-rtags)
+(require 'irony)
+(require 'company-irony)
+(require 'company-c-headers)
+(require 'rtags)
+(require 'cc-mode)
 
 (add-hook 'c-mode-common-hook
           (lambda ()
             (when (derived-mode-p 'c-mode 'c++-mode)
-                ;; (cppcm-reload-all)
-                (flycheck-mode)
                 (irony-mode)
+                (flycheck-mode)
                 (flycheck-select-checker 'rtags))))
 
-; Avoid typing full path when starting gdb
-;; (global-set-key (kbd "C-c C-g")
-;;                 '(lambda ()(interactive)
-;;                    (gud-gdb (concat "gdb --fullname "
-;;                                     (cppcm-get-exe-path-current-buffer)))))
-
-;; Specify extra flags forwarded to compiler
-;(setq cppcm-extra-preprocss-flags-from-user '("-I/usr/src/linux/include" "-DNDEBUG"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; rtags kicks ass
@@ -26,18 +26,21 @@
 (eval-after-load 'company
   '(progn
      (add-to-list 'company-backends 'company-irony)
-     ;(add-to-list 'company-backends 'company-rtags)
-     ; ensure we don't use company-clang by mistake
+     (add-to-list 'company-backends 'company-c-headers)
+     ;(add-to-list 'company-backends 'company-rtags)  ; doesn't work that well
+     ; ensure we don't use some company-whatever by mistake
      (setq company-backends (delete 'company-clang company-backends))
      (setq company-backends (delete 'company-semantic company-backends))))
 
-(define-key c-mode-base-map (kbd "s-<mouse-1>")
-  (mbd-my-func-mouse rtags-find-symbol-at-point))
-(define-key c-mode-base-map (kbd "M-.") #'rtags-find-symbol-at-point)
-
+(eval-after-load 'c-mode
+  '(progn
+     (define-key c-mode-base-map (kbd "s-<mouse-1>")
+       (mbd-my-func-mouse rtags-find-symbol-at-point))
+     (define-key c-mode-base-map (kbd "M-.") #'rtags-find-symbol-at-point)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; cmake-ide should make my life easier... But it doesn't
+
 (cmake-ide-setup)
 (setq cmake-ide-build-pool-dir "~/Devel/builds")
 (setq cmake-ide-build-pool-use-persistent-naming t)
