6dd10cb1eaf8ae45dd7e74647c6cde7ded7ee0d8
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-04-30 22:03:06 +0200

    Update packages

--- elpa/conda-20161103.743/conda-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "conda" "20161103.743" "Work with your conda environments" '((emacs "24.4") (pythonic "0.1.0") (dash "2.13.0") (s "1.11.0") (f "0.18.2")) :url "http://github.com/necaris/conda.el" :keywords '("python" "environment" "conda"))
