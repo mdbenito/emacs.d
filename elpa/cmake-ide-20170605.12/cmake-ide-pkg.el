8d3954a245a8a8e918c6ba1a6956f32e0c7cd272
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-07-06 10:14:51 +0200

    Update 24 packages

--- elpa/cmake-ide-20170605.12/cmake-ide-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cmake-ide" "20170605.12" "Calls CMake to find out include paths and other compiler flags" '((emacs "24.1") (cl-lib "0.5") (seq "1.11") (levenshtein "0")) :commit "b7bb7bf36d442c377988a090fc648b03b50c8eff" :url "http://github.com/atilaneves/cmake-ide" :keywords '("languages"))
--- elpa/ein-20170606.1325/ein-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "ein" "20170606.1325" "Emacs IPython Notebook"
-  '((websocket "1.7")
-    (request "0.3")
-    (deferred "0.5")
-    (cl-generic "0.3")
-    (dash "2.13.0")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
