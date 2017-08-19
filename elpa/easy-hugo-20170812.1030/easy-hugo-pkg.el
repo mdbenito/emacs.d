648edce68ece7dda981a289b1c89d4b038420895
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-08-19 10:21:12 +0200

    Update several packages

---
+++ elpa/easy-hugo-20170812.1030/easy-hugo-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "easy-hugo" "20170812.1030" "Write blogs made with hugo by markdown or org-mode" '((emacs "24.4")) :commit "69c7c0df9c3d8b97b5a779c41460f6fb4f65ac75" :url "https://github.com/masasam/emacs-easy-hugo")
---
+++ elpa/websocket-20170612.2053/websocket-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "websocket" "20170612.2053" "Emacs WebSocket client and server" '((cl-lib "0.5")) :commit "23ec19933d54f135ae10ccd593acc01a52740e2c" :keywords '("communication" "websocket" "server"))
