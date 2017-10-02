ecdf49db4b5c82d68ad7cb98288ce4622cdce7a0
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-10-02 12:45:39 +0200

    Update 41 packages

---
+++ elpa/cython-mode-20170907.1648/cython-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cython-mode" "20170907.1648" "Major mode for editing Cython files" 'nil :commit "1c4f32b93570b41de3c8028711549a32e15b31f1")
---
+++ elpa/easy-hugo-20171001.1327/easy-hugo-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "easy-hugo" "20171001.1327" "Write blogs made with hugo by markdown or org-mode" '((emacs "24.4")) :commit "939f025e86d82afeb2ff7a71ce9c38536e756699" :url "https://github.com/masasam/emacs-easy-hugo")
